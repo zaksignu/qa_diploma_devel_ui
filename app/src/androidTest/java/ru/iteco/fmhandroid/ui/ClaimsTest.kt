dfa5c31abecf466bf5c5bff8bf98ea6872185c95
zaksignu <zaksignu@gmail.com>
2023-06-05 08:06:21 +0300

    espresso test

--- app/src/androidTest/java/ru/iteco/fmhandroid/ui/ClaimsTest.kt
+++ app/src/androidTest/java/ru/iteco/fmhandroid/ui/ClaimsTest.kt
@@ -2,32 +2,40 @@ package ru.iteco.fmhandroid.ui
 
 
 //import androidx.test.ext.junit.runners.AndroidJUnit4
-import android.content.ContentValues.TAG
+//import androidx.test.espresso.action.ViewActions.*
+
 import android.content.Context
 import android.content.Intent
-import android.util.Log
+import androidx.recyclerview.widget.RecyclerView
+import androidx.test.core.app.ActivityScenario
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.espresso.Espresso.*
-import androidx.test.espresso.action.ViewActions.*
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.contrib.*
 import androidx.test.espresso.matcher.ViewMatchers
 import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.rule.ActivityTestRule
 import androidx.test.uiautomator.*
 import org.junit.Assert.*
 import org.junit.Before
+import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
+import ru.iteco.fmhandroid.R
 import ru.iteco.fmhandroid.ui.datawizard.Datawizard
 
-//import org.junit.Test
-//import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
 class ClaimsTest {
-    private lateinit var device: UiDevice;
-    private lateinit var dWizard: Datawizard;
+    private lateinit var device: UiDevice
+    private lateinit var dWizard: Datawizard
+
+//    @Rule
+//     val  activityTestRule: ActivityTestRule<AppActivity> = ActivityTestRule(AppActivity::class.java)
+
 
     @Before
     fun startMainActivityFromHomeScreen() {
@@ -39,7 +47,7 @@ class ClaimsTest {
         device.pressHome()
 
         // Wait for launcher
-        val launcherPackage = device.launcherPackageName;
+        val launcherPackage = device.launcherPackageName
         //assertThat(launcherPackage, CoreMatchers.notNullValue())
 
         device.wait(
@@ -48,8 +56,13 @@ class ClaimsTest {
         )
         // Launch the blueprint app
         val context = ApplicationProvider.getApplicationContext<Context>()
-        val packageName = context.packageName;
+
+
+        val packageName = context.packageName
         val intent = context.packageManager.getLaunchIntentForPackage(packageName)
+
+
+
         intent!!.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK) // Clear out any previous instances
 
         context.startActivity(intent)
@@ -66,7 +79,7 @@ class ClaimsTest {
         )
         try {
 
-            dWizard.mainTradeMark(device)!!.isEnabled()
+            dWizard.mainTradeMark(device)!!.isEnabled
 
         } catch (e: UiObjectNotFoundException) {
             dWizard.authLoginField().text = dWizard.LOGIN_CORRECT
@@ -92,7 +105,7 @@ class ClaimsTest {
 
         dWizard.mainMenu(device)!!.click()
         dWizard.nameClaim().click()
-        device.waitForIdle(500L);
+        device.waitForIdle(500L)
         assertTrue(dWizard.nameClaim().exists())
 
     }
@@ -102,7 +115,7 @@ class ClaimsTest {
     fun mainMenuAllClaims() {
         dWizard.mainAllClaims(device)!!.click()
 
-        assertTrue(dWizard.allClaimCardInList(device, 0)!!.exists());
+        assertTrue(dWizard.allClaimCardInList(device, 0)!!.exists())
     }
 
     // Тест 4.1
@@ -170,7 +183,7 @@ class ClaimsTest {
     fun claimsOneCard() {
         dWizard.mainAllClaims(device)!!.click()
         dWizard.allClaimCardInList(device, 0)!!.click()
-        assertTrue(dWizard.allClaimCardInList(device, 0)!!.exists());
+        assertTrue(dWizard.allClaimCardInList(device, 0)!!.exists())
     }
 
     @Test
@@ -179,7 +192,7 @@ class ClaimsTest {
 
         onView(ViewMatchers.withId(R.id.description_material_text_view))
             .perform(
-                RecyclerViewActions.actionOnItemAtPosition(
+                RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(
                     0,
                     click()
                 )
